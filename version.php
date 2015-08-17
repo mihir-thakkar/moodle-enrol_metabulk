d685c3e3cffc7ed0b5bfdad266cbb0b69865038d
Mihir Thakkar <mihir9293@gmail.com>
2015-08-17 07:32:05 +0530

    adding version details

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2015052116;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;        // Requires this Moodle version
 $plugin->component = 'enrol_metabulk';  // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1';
