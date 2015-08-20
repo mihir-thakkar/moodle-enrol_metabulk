41b946cbef78205e5d4fcb67d64b627af85fe581
Mihir Thakkar <mihir9293@gmail.com>
2015-08-20 08:25:05 +0530

    change supported version to '2.8

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015052116;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015051100;        // Requires this Moodle version
+$plugin->requires  = 2014101000;        // Requires this Moodle version
 $plugin->component = 'enrol_metabulk';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1';
